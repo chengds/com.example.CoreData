c76df22df974274e1f1e680ea38afdf801edeb62
Arvi Punzalan <arvi1222@yahoo.com>
2015-07-25 17:02:36 +0900

    just have to commit

--- CoreData/ViewController.swift
+++ CoreData/ViewController.swift
@@ -23,7 +23,7 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
-        println("meow")
+        print("meow")
     }
 
 
